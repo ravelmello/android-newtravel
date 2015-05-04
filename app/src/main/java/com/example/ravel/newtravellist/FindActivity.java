946d4485dfc34ed29822fdbdd3598822dfa81c35
ravelmello <ravelmello3@gmail.com>
2015-05-04 18:29:04 -0300

    Commit das telas posteriores ao login

--- app/src/main/java/com/example/ravel/newtravellist/FindActivity.java
+++ app/src/main/java/com/example/ravel/newtravellist/FindActivity.java
@@ -6,33 +6,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 
-public class MenuActivity extends Activity {
+public class FindActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_find);
 
-        ImageView imageBtn = (ImageView) findViewById(R.id.imageViewPoint);
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
 
-        imageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MenuActivity.this, FindActivity.class));
-            }
-        });
+
+
+        if(searchView.getQuery().toString() != null){
+
+            startActivity(new Intent(FindActivity.this, MapActivity.class));
+
+        }else {
+
+            String textErro = "Digite algo válido";
+
+            Toast toast = Toast.makeText(FindActivity.this, textErro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_find, menu);
         return true;
     }
 
--- app/src/main/java/com/example/ravel/newtravellist/MapActivity.java
+++ app/src/main/java/com/example/ravel/newtravellist/MapActivity.java
@@ -1,38 +1,24 @@
 package com.example.ravel.newtravellist;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
 
-public class MenuActivity extends Activity {
+public class MapActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-        ImageView imageBtn = (ImageView) findViewById(R.id.imageViewPoint);
-
-        imageBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MenuActivity.this, FindActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_map);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
